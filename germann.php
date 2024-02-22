c930d874665bbfd23a79afc696556a8db85d3f75
Magaly12335 <160169360+Magaly12335@users.noreply.github.com>
2024-02-21 18:41:10 -0600

    Add files via upload

---
+++ germann.php
@@ -0,0 +1,24 @@
+<html>
+    <?php  
+    $mango=array(12, 45, 6, 2, 78, 4, 23, 5);
+    
+
+    $a=count($mango);
+    echo $mango[8];
+    for($i=0; $i<=$a; $i++)
+    {
+        echo $mango[$i] . ", ";
+    }
+    
+
+
+
+
+
+     
+
+
+    ?>
+
+
+</html>
